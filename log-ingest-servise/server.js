709aaf4df81c81b1756b8de862385bdc11c89e2d
Nitzachon <nitzachon.azani@intel.com>
2024-09-30 18:36:50 +0300

    temp

--- log-ingest-servise/server.js
+++ log-ingest-servise/server.js
@@ -1,4 +1,4 @@
-const express = require("express");
+/*const express = require("express");
 const app = express ();
 
 var http = require("http").createServer(app);
@@ -15,4 +15,42 @@ webSocket('connection', (socket) => {
         console.log("message recieve")
     })
 })
+*/
+/*
 
+
+
+*/
+const fs = require('fs');
+const readline = require('readline');
+
+async function readSocked() {
+  const fileStream = fs.createReadStream('log_example.log');
+
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity
+  });
+
+    var reg = /\w+|\((.*?)\)/g; // extract data
+    var match;
+    var res = [];
+
+    var currentdate = new Date();
+  for await (const line of rl) {
+    // Each line in input.txt will be successively available here as `line`.
+    console.log(`${line}`);
+    while (match = reg.exec(line)) {
+        res.push(match[1] || match[0]);
+    }
+    console.log(`${res}`);
+    /*send to ES!
+        send json:
+        {
+            service_name: match[1],
+            date_time: currentdate
+        }
+     */
+  }
+}
+readSocked();
--- log-processing-service/README.md
+++
@@ -1,6 +0,0 @@
-# Log processing service
-retrieve logs from  a variaty of application that send logs outside
-
-
-
-
