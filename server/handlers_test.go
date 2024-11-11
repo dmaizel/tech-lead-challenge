e978b7e6419e7e769290b826e828df29e20062c7
chenk <hen.keinan@gmail.com>
2024-11-11 13:41:07 +0200

    feat: update status
    Signed-off-by: chenk <hen.keinan@gmail.com>

--- server/handlers_test.go
+++ server/handlers_test.go
@@ -2,9 +2,11 @@ package server
 
 import (
 	"encoding/json"
+	"io"
 	"net/http"
 	"strings"
 	"tech-lead-challenge/db"
+	"tech-lead-challenge/service/logparser"
 	"tech-lead-challenge/test"
 	"testing"
 
@@ -28,5 +30,14 @@ func TestFetchLogs(t *testing.T) {
 	presolveRes, err := test.InvokeRequestWithResponse(req, h.FetchLogs, "/logs")
 	assert.NoError(t, err)
 	assert.True(t, presolveRes.Code == http.StatusOK)
+	resData, err := io.ReadAll(presolveRes.Body)
+	assert.NoError(t, err)
+	var logs []logparser.Log
+	err = json.Unmarshal(resData, &logs)
+	assert.NoError(t, err)
+	assert.Equal(t, logs[0].Data, "data")
+	assert.Equal(t, logs[0].TimeStamp, "2024-01-02 15:09:05.000")
+	assert.Equal(t, logs[0].ServiceName, "test")
+	assert.Equal(t, logs[0].Level, "Info")
 
 }
